7f2a54146c8842d821e23d2e22169a3dd337eebb
Daniel Rocha <rochadaniel15@gmail.com>
2019-08-29 20:10:14 -0300

    adicionado a sintaxe de Arrow Function do ES6 no componente exportado

--- src/pages/Login/elements/Header.js
+++ src/pages/Login/elements/Header.js
@@ -4,17 +4,16 @@ import {Tabs} from '../../../components/Tab';
 import './style/Header.css'
 import './style/Tabs.css'
 
-export default function Header(){
-    return(
-        <header id='header-login'>
-            <img className='logo' src='#' alt=''/>
-            <h1>Sistema Informatizado de Saúde.</h1>    
-            <Tabs className = 'nav-tabs-login'>
-                {[
-                    {name : 'login'},
-                    {name : 'help'},
-                ]}
-            </Tabs>                
-        </header>
-    );
-}
+export default () =>
+(
+    <header id='header-login'>
+        <img className='logo' src='#' alt=''/>
+        <h1>Sistema Informatizado de Saúde.</h1>    
+        <Tabs className = 'nav-tabs-login'>
+            {[
+                {name : 'login'},
+                {name : 'help'},
+            ]}
+        </Tabs>                
+    </header>
+);
