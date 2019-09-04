18cb4c279b901e3d5b2f5059e5677e5b21519c38
Daniel Rocha <rochadaniel15@gmail.com>
2019-09-04 12:02:53 -0300

    correção na animação dos inputs do formulário

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Form.css'
 
 
 
-const Input = ({name, type, placeholder, value, id, className, label, func}) =>
-(
-    <label> 
-        <input
-            onChange = {func}
-            name = {name} 
-            type = {type || ''}
-            placeholder = {placeholder || ''}
-            value = {value || ''}
-                
-        />
-        <span>{label}</span>
-    </label>
-);
+const Input = ({name, type, placeholder, value, id, className, label, func}) =>{
+    
+    const [styleFillInput, setStyleFillInput ] = useState("empty-style-input");
+    
+    const toggleStyleInput = () => (value === "" && styleFillInput !== "empty-style-input")
+        ? setStyleFillInput("empty-style-input")
+        : setStyleFillInput("fill-style-input");
 
+    return (
+        <label> 
+            <input
+                
+                onChange = {func}
+                onFocus = {toggleStyleInput}
+                onBlur = {toggleStyleInput}
+                name = {name} 
+                type = {type || ''}
+                placeholder = {placeholder || ''}
+                value = {value || ''}
+                    
+            />
+            <span className = {styleFillInput}>{label}</span>
+        </label>
+    );
+}
 const Form = ({classNames ,func, method, children}) =>
 (
     <form
