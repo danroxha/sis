a8488d2b300112dd497f26702d68139b2fc2e827
Daniel Rocha <rochadaniel15@gmail.com>
2019-09-06 01:30:46 -0300

    update

--- src/pages/DashBoard/elements/Tools.js
+++ src/pages/DashBoard/elements/Tools.js
@@ -1,42 +1,17 @@
 import React from 'react';
-import {Input, Form} from '../../../components/Form';
-import {Button} from '../../../components/Button';
+
+import SectionPrimary from './components/SectionPrimary';
+import SectionSecondary from './components/SectionSecondary';
+import SectionTertiary from './components/SectionTertiary';
 
 import './style/Tools.css';
 
 export default function Tools(){
     return(
         <main id={'tools-section'}>
-            <section>
-                <Button>
-                    <img src="#" alt=""/>
-                    <span>Cadastrar Paciente</span>
-                </Button>
-                
-                <Button>
-                    <img src="#" alt=""/>
-                    <span>Calendario Profissional</span>
-                </Button>
-
-                <Button>
-                    <img src="#" alt=""/>
-                    <span>Campanhas</span>
-                </Button>
-            </section>
-            <section>
-                <Form>
-                    <Input label='cpf'/>
-                    <Input label='nome'/>
-                    <Button>Pesquisar</Button>
-                </Form>
-            </section>
-            <section>
-                <Form>
-                    <Input label='cpf'/>
-                    <Input label='nome'/>
-                    <Button>Pesquisar</Button>
-                </Form>
-            </section>
+            <SectionPrimary />
+            <SectionSecondary />
+            <SectionTertiary />
         </main>
     );
 }
