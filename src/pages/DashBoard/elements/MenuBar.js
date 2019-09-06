76f58173e33d318dd60809ad38a8dea11b27e6b2
Daniel Rocha <rochadaniel15@gmail.com>
2019-09-05 23:18:48 -0300

    Criação inciais do Menu lateral

---
+++ src/pages/DashBoard/elements/MenuBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style/MenuBar.css';
+
+export default function MenuBar(){
+    return(
+        <nav className={'nav-bar-menu'}>
+                
+        </nav>
+    );
+}
