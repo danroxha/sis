8bde2b5c76d490e0a28aee001c94251b32b3d6be
Daniel Rocha <rochadaniel15@gmail.com>
2019-09-08 00:08:21 -0300

    Correção nos compoenentes globais pra deixar mais generico

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,24 +2,26 @@ import React, {useState} from 'react';
 import './Form.css'
 
 
-
-const Input = ({name, type, placeholder, value, id, className, label, func}) =>{
+const Input = ({name, type, placeholder, value, id, classNamesInput, label, func}) =>{
     
     const [styleFillInput, setStyleFillInput ] = useState("empty-style-input");
     
-    const toggleStyleInput = () => (value === "" && styleFillInput !== "empty-style-input")
+    const toggleStyleInput = () =>
+        ((value === "" || value === undefined) && styleFillInput !== "empty-style-input")
         ? setStyleFillInput("empty-style-input")
         : setStyleFillInput("fill-style-input");
 
     return (
-        <label> 
+        <label
+            className = {'-label-input-component'}
+        > 
             <input
-                
-                onChange = {func}
+                className = {classNamesInput || '-input-default-style-component-global'}
+                onChange = {func || (() => {})}
                 onFocus = {toggleStyleInput}
                 onBlur = {toggleStyleInput}
-                name = {name} 
-                type = {type || ''}
+                name = {name || ''} 
+                type = {type || 'text'}
                 placeholder = {placeholder || ''}
                 value = {value || ''}
                     
@@ -30,13 +32,32 @@ const Input = ({name, type, placeholder, value, id, className, label, func}) =>{
 }
 const Form = ({classNames ,func, method, children}) =>
 (
-    <form
-        className = {`${classNames}`} 
-        onSubmit = {func || ''}
-        method = {method || ''}
+    <form 
+        className = {classNames || ''}
+        onSubmit = {func || (()=>{})}
+        method = {method || 'POST'}
     >
-        {children}
+        {children || ''}
     </form>
 );
 
-export {Input, Form};
+const Select = ({data, classNames}) => (
+    <select 
+            className={ classNames ||'-select-input-default-style-component-global'}
+        >
+            {data 
+                ? data.map((value, index) =>
+                    (   <option
+                            key = {`${value.name + index}`} 
+                            value={`${value.name}`}
+                        >
+                            {`${value.name}`}
+                        </option>
+                    )
+                )
+                : ''
+        }
+        </select>
+);
+
+export {Input, Form, Select,};
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,31 +1,21 @@
 import React from 'react';
-
+import {Form, Input, Select} from '../Form/';
 import './Search.css';
 
-const Search = ({placeholder, data}) => (
-    <form>
-        <input 
-            type = 'text'
-            placeholder = {placeholder || ''}
-            className = {'search-input -common'}
+const Search = ({placeholder, data}) => 
+(
+    <Form>
+        <Input 
+            classNamesInput = {'search-input -common'}
+            type={'text'}
+            placeholder={placeholder}
+           
         />
-        <select 
-            className= {'search-select -common'}
-        >
-            {data 
-                ? data.map((value, index) =>
-                    (   <option
-                            key = {`${value.name + index}`} 
-                            value={`${value.name}`}
-                        >
-                            {`${value.name}`}
-                        </option>
-                    )
-                )
-                : ''
-        }
-        </select>
-    </form>
+        <Select 
+            data = {data}
+            classNames = {'search-select -common'}
+        />
+    </Form>
 );
 
 export default Search;
